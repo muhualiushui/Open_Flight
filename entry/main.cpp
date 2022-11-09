8a6a1b63dcfb1ac3c02833ca0c64405022b3f1ad
Tongrui-Yu-tongrui4 <90438125+Tongrui-Yu-tongrui4@users.noreply.github.com>
2022-11-08 23:53:52 -0600

    Changed the include and add more test case

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,8 +1,10 @@
-#include "../includes/Transform.h"
+#include "../src/Transform.cpp"
 
 int main(){
     Transform data;
     data.InsertAirports("../Data/airports.dat");
-    data.print(100,110);
+    data.InsertAirlines("../Data/airlines.dat");
+    data.printAirports(100,110);
+    data.printAirlins(0,10);
     return 0;
 }
