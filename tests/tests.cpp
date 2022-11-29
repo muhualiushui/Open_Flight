45151147ddfd257ba3b06f4ed7b5811539acaf55
Tongrui-Yu-tongrui4 <90438125+Tongrui-Yu-tongrui4@users.noreply.github.com>
2022-11-28 22:41:58 -0600

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,8 @@
 #include "../includes/Flight_map.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-
+#include <fstream>
+using namespace std;
 TEST_CASE("data") {
     Transform data;
     data.InsertAirports("../Data/airports.dat");
@@ -11,4 +12,32 @@ TEST_CASE("data") {
     data.printAirlins(0,10);
     // data.printRoutes(10,20);
 
+}
+
+TEST_CASE("BFS itself") {
+    Flight_map *map = new Flight_map("../Data/airports.dat", "../Data/routes.dat", "../Data/airlines.dat");
+    vector<int> result = map->findPath(3830,3830);
+    vector<int> toCompare = {3830};
+    REQUIRE(result == toCompare);
+}
+
+TEST_CASE("BFS out of data") {
+    Flight_map *map = new Flight_map("../Data/airports.dat", "../Data/routes.dat", "../Data/airlines.dat");
+    vector<int> result = map->findPath(14900,20000);
+    vector<int> toCompare;
+    REQUIRE(result == toCompare);
+}
+
+TEST_CASE("BFS successfully") {
+    Flight_map *map = new Flight_map("../Data/airports.dat", "../Data/routes.dat", "../Data/airlines.dat");
+    vector<int> result = map->findPath(3830,4042);
+    vector<int> toCompare;
+    ifstream TheText("../tests/BFS_Sucess.txt");
+    string word;
+    if (TheText.is_open()) {
+        while (getline(TheText, word)) {
+            toCompare.push_back(stoi(word));
+        }
+    }
+    REQUIRE(result == toCompare);
 }
